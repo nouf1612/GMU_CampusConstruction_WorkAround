8c53a7647a736e2a9a502925f8852d3de7d71d0f
nouf1612 <nouf16.12@gmail.com>
2020-04-21 12:07:02 -0400

    Fixed the spinners in Quick_Route

--- app/src/main/java/com/example/gmu_campusconstruction_workaround/Quick_Route.java
+++ app/src/main/java/com/example/gmu_campusconstruction_workaround/Quick_Route.java
@@ -9,16 +9,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
 public class Quick_Route extends AppCompatActivity {
 
+    //variables for the route database, list spinners and their adapters,
+    // and the MainActivity button
     private RoutesDatabase routeDb;
-    private Spinner spinner_SB;
-    private Spinner spinner_DB;
+    private Spinner spinner_SB, spinner_DB;
     private Button MAButton;
+    private String[] UP, LP, MP;
+    private String Building_1,Building_2;
+    private ArrayAdapter<String> SB_adapter,DB_adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +33,59 @@ public class Quick_Route extends AppCompatActivity {
         ConfigureMAButton();
 
 
-        // Layout for main screen
-        Spinner Spinner_SB = (Spinner) findViewById(R.id.spinner_Start_Building);
-        Spinner Spinner_DB = (Spinner) findViewById(R.id.spinner_Dest_Building);
-        ArrayAdapter<String> SB_adapter;
-    }
+        // spinners for the start and destination lists
+        spinner_SB = (Spinner) findViewById(R.id.spinner_Start_Building);
+        spinner_DB = (Spinner) findViewById(R.id.spinner_Dest_Building);
+        SB_adapter = new ArrayAdapter<String>(Quick_Route.this,
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.Start_Buildings));
+        spinner_SB.setAdapter(SB_adapter);
 
+
+        // Destination spinner conditions
+        //Set arrays for comparison
+        UP = getResources().getStringArray(R.array.Upper_Part);
+        LP = getResources().getStringArray(R.array.Lower_Part);
+        MP = getResources().getStringArray(R.array.Middle_Part);
+
+
+
+        spinner_SB.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String User_Choice = parent.getItemAtPosition(position).toString();
+                // figure out what list the users choice belongs to
+                int list_num = ListNum(User_Choice);
+
+                //set the destination adapter based on the users choice
+                if (list_num==1) {// belongs in the upper part array
+                    DB_adapter = new ArrayAdapter<String>(Quick_Route.this,
+                            android.R.layout.simple_spinner_item,
+                            getResources().getStringArray((R.array.UP_Dest)));
+                    spinner_DB.setAdapter(DB_adapter);   }
+                else if (list_num==2 || list_num==3) {
+                    DB_adapter = new ArrayAdapter<String>(Quick_Route.this,
+                            android.R.layout.simple_spinner_item,
+                            getResources().getStringArray((R.array.Upper_Part)));
+                    spinner_DB.setAdapter(DB_adapter);
+                }
+                else {
+                    //show error message
+                    showMessage("Error", "Nothing found");
+                    }
+                Building_1 = User_Choice;
+                //TEST IF THIS SHOWS THE RIGHT LIST OR NOT THEN FIX THE CHOICE FOR BUILDING_2
+
+
+            }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
 
 
     /**
@@ -49,6 +101,93 @@ public class Quick_Route extends AppCompatActivity {
             }
         });
     }
+
+    /**
+     * Check if the users choice belongs to List
+     *
+     * @param UserChoice check if in List
+     * @param List       check if the user choice is in it
+     * @return true if the users choice is in List, false otherwise
+     */
+    private boolean CheckList(String UserChoice, String[] List) {
+        boolean belongs_to_current_list = false;
+        //see if user choice belongs to List
+        for (int i = 0; i < List.length; i++) {
+            if (UserChoice.equals(List[i])) {
+                belongs_to_current_list = true;
+                break;
+            }
+        }
+
+        return belongs_to_current_list;
+    }
+
+    /**
+     * figure out what list the users choice belongs in
+     *
+     * @param UserChoice check which list it belongs to
+     * @return Lists number
+     */
+    private int ListNum(String UserChoice) {
+        int list_num = 0;
+        boolean cond = false;
+        // check which list the users choice belongs in
+        //upper = 1, middle = 2, lower = 3
+        for (int i = 1; i <= 3; i++) {
+            if (i == 1) {
+                cond = CheckList(UserChoice, UP);
+            } else if (i == 2) {
+                cond = CheckList(UserChoice, MP);
+            } else {
+                cond = CheckList(UserChoice, LP);
+            }
+
+            if (cond) {
+                list_num = i;
+                break;
+            }
+        }
+        return list_num;
+    }
+
+    /**
+     *
+     * @param title
+     * @param Message
+     */
+    public void showMessage(String title, String Message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+
+    /**
+     * NOT DONE NEEDS EDITING
+     * @param Building_1
+     * @param Building_2
+     *//*
+    public void viewRoute(String Building_1, String Building_2) {
+        Cursor res = routeDb.getAllData();
+        String route = "none";
+        if(res.getCount() == 0) {
+                //show error message
+                showMessage("Error", "Nothing found");
+            }
+            StringBuffer buffer = new StringBuffer();
+            while(res.moveToNext()) {
+                if(res.getString(1).equals(building)) {
+                    buffer.append(res.getString(2) + "\n");
+                }
+            }
+            //show table
+            showMessage("Here! Follow this route.", buffer.toString());
+        }*/
+
+
+
+}
 
     /* Old Layout by kevin
 
@@ -186,11 +325,5 @@ public class Quick_Route extends AppCompatActivity {
         );
     }
 
-    public void showMessage(String title, String Message) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setCancelable(true);
-        builder.setTitle(title);
-        builder.setMessage(Message);
-        builder.show();
-    }*/
-}
+
+}*/
