5dbd7ccfb5a737b34980d4cfea2cd4a32388d9b9
nouf1612 <nouf16.12@gmail.com>
2020-04-23 20:20:07 -0400

    changed buttons on the main screen

--- app/src/main/java/com/example/gmu_campusconstruction_workaround/MainActivity.java
+++ app/src/main/java/com/example/gmu_campusconstruction_workaround/MainActivity.java
@@ -1,24 +1,11 @@
 package com.example.gmu_campusconstruction_workaround;
 
-import android.app.AlertDialog;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
      * to the Quick Route activity
      */
     private void ConfigureQRButton() {
-        Button QRButton = (Button) findViewById(R.id.button_GoToQR);
+        ImageButton QRButton = findViewById(R.id.button_GoToQR);
         QRButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
      * to the Schedule Route activity
      */
     private void ConfigureSRButton() {
-        Button SRButton = (Button) findViewById(R.id.button_GoToSR);
+        ImageButton SRButton = findViewById(R.id.button_GoToSR);
         SRButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
      * to the Favorite List activity
      */
     private void ConfigureFLButton() {
-        Button QRButton = (Button) findViewById(R.id.button_GoToFL);
+        ImageButton QRButton = findViewById(R.id.button_GoToFL);
         QRButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
